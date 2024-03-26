a352b3d87f14df8462b6a64816f9e4ac3024c569
Mohammad Tasif Hossan <153765460+tasifhossan@users.noreply.github.com>
2024-03-26 19:17:40 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+	$('.counter1').counterUp({
+    delay: 10,
+    time: 1500
+    });
+
+    $('.counter2').countdown360({
+    	radius      : 60,
+	    seconds     : 50,
+	    fontColor   :'#FFFFFF',
+	    onComplete  :function () { console.log('done') }
+
+    });
+
+    $('.svg-test').svgTimer({
+    	time: 60,
+    });
+
+    $('.counter3').countUp();
+
+    $('.counter4').numScroll({
+    	number: 123456,
+    	time: 2000,
+    });
+
+});
